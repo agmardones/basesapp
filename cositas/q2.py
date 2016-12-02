02a301a17780222c2326f467a1eac67ae3a00d21
Andres Mardones <agmardones@uc.cl>
2016-12-02 02:09:13 -0300

    q2 :rocket:

---
+++ cositas/q2.py
@@ -0,0 +1,52 @@
+from pymongo import MongoClient
+import time
+import sys
+
+MONGODATABASE = "entrega4"
+MONGOSERVER = "localhost"
+MONGOPORT = 27017
+client = MongoClient(MONGOSERVER, MONGOPORT)
+db = client[MONGODATABASE]
+
+args1 = sys.argv[1:3] # Nombre del primero
+args2 = sys.argv[3:] # Nombre del segundo
+
+print(args1)
+print(args2)
+users1 = db.users.find({'name': '{} {}'.format(*args1)})
+users2 = db.users.find({'name': '{} {}'.format(*args2)})
+try:
+	user1 = users1.next()
+	user2 = users2.next()
+	user1id = user1["id"]
+	user2id = user2["id"]
+	msgs12 = db.messages.find({'sender': user1id, 'receptant': user2id})
+	msgs21 = db.messages.find({'sender': user2id, 'receptant': user1id})
+	"""
+	Supongamos que nuestro bello y querido usuario quiere buscar entre las fechas
+	2015-04-17 y 2016-12-01
+	(Habría que poner un if pq podría no querer filtrar por fechas)
+	"""
+	inf = time.strptime("2015-04-17", "%Y-%m-%d") # Límite inferior
+	sup = time.strptime("2016-12-01", "%Y-%m-%d") # Límite superior
+	"""
+	Supongamos tambien que quiere los que están en la posición:
+	latitud = -18.483333
+	longitud = -70.333333
+	"""
+	lat = -18.483333 
+	lon = -70.333333
+
+	for m in msgs12:
+		if inf < time.strptime(m["date"], "%Y-%m-%d") < sup: pass
+			# print(m)
+		if float(m["long"]) == lon and float(m["lat"]) == lat:
+			print(m)
+	for m in msgs21:
+		if inf < time.strptime(m["date"], "%Y-%m-%d") < sup: pass
+			# print(m)
+		if float(m["long"]) == lon and float(m["lat"]) == lat:
+			print(m)
+
+except StopIteration:
+	print("Probablemente una consulta vacía")
