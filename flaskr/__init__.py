5e8a8a51ba0d8f83befb12211738f0ed5e005ca5
Matias Eynaudi <Matias Eynaudi>
2016-12-01 22:59:45 -0300

    :alien:

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -54,9 +54,9 @@ def mongo():
     query = request.args.get("query")
     results = eval('mongodb.'+query)
     results = json_util.dumps(results, sort_keys=True, indent=4)
-    nombre = request.args.get("frame")
+    nombre = request.args.get("fname")
     if "find" in query:
-        return render_template('mongo.html', results=results, name=nombre)
+        return render_template('mongo.html', results=results, name="Nombre")
     else:
         return "ok"
 
